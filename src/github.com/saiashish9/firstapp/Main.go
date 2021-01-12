99c0eff45c1cd87c5b6a6470fabf39ee8123f768
saiashish9 <saiashish3760@gmail.com>
2021-01-12 18:08:32 +0530

    variables updated

--- src/github.com/saiashish9/firstapp/Main.go
+++ src/github.com/saiashish9/firstapp/Main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+//  i:=42 X
+var i int = 42
+
 func main() {
-	fmt.Println("Hello Go!")
+	// var i int
+	// i = 42
+	// var i int = 42
+	// i := 42
+	// var j float32 = 27
+	// go compiler can
+	// automatically figure out
+	// the datatype itself
+	// fmt.Println(i)
+	// fmt.Printf("%v ,%T", j, j)
+
+	fmt.Printf("%v ,%T", i, i)
 }
