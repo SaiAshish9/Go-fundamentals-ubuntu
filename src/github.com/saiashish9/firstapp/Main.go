305ad23f37609d0a01582ab0034cf42c69a9507f
saiashish9 <saiashish3760@gmail.com>
2021-01-15 02:21:51 +0530

    go routines updated

--- src/github.com/saiashish9/firstapp/Main.go
+++ src/github.com/saiashish9/firstapp/Main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+var counter = 0
+var a = sync.RWMutex{}
+
 func main() {
 
+	// runtime.GOMAXPROCS(100)
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		go sayHello()
+		go increment()
+	}
+	wg.Wait()
+}
+
+func sayHello() {
+	a.RLock()
+	fmt.Println(counter)
+	a.RUnlock()
+	wg.Done()
+}
+
+func increment() {
+	a.Lock()
+	counter++
+	a.Unlock()
+	wg.Done()
 }
---
+++ src/github.com/saiashish9/firstapp/SingleGoRoutine.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// creating goroutines
+// synchronization waitgroups mutexes
+// parallelism
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	// go sayHello()
+	// to spin up a new thread and run a new thread in that function
+	// time.Sleep(100 * time.Millisecond)
+	var msg = "hello"
+	wg.Add(1)
+	go func(msg string) {
+		fmt.Println(msg)
+		wg.Done()
+	}(msg)
+	msg = "Good Bye"
+	wg.Wait()
+	// closure's anonymous fns do have access to outer scope
+	// time.Sleep(100 * time.Millisecond)
+}
+
+func sayHello() {
+	fmt.Println("hello")
+}
