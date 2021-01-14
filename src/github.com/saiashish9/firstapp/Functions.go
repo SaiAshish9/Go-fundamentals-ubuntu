9bb740b9ae8132214dcd71c757c378cb1dd0f464
saiashish9 <saiashish3760@gmail.com>
2021-01-14 23:51:48 +0530

    fns updated

---
+++ src/github.com/saiashish9/firstapp/Functions.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	// sayMsg("Go")
+	// greeting := "Hello"
+	// name := "Sai"
+	// sayGreeting(&greeting, &name)
+	// fmt.Println(name)
+	// sum(1, 2, 3, 4)
+
+	g := greeter{
+		"hi", "Go",
+	}
+	g.greet()
+	fmt.Println(g.name)
+}
+
+type greeter struct {
+	greeting string
+	name     string
+}
+
+func (g *greeter) greet() {
+	fmt.Println(g)
+	g.name = ""
+}
+
+func sum(values ...int) {
+	fmt.Println(values)
+	result := 0
+	for _, v := range values {
+		result += v
+	}
+	fmt.Println(result)
+}
+
+func sayGreeting(greeting, name *string) {
+	fmt.Println(*greeting, *name)
+	*name = "Sai9"
+	fmt.Println(*name)
+}
+
+// func sayMsg(msg string) {
+// 	fmt.Println(msg)
+// }
--- src/github.com/saiashish9/firstapp/Main.go
+++ src/github.com/saiashish9/firstapp/Main.go
@@ -1,58 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-)
-
 func main() {
-	// a, b := 1, 0
-	// ans := a / b
-	// fmt.Println(ans)
-
-	fmt.Println("start")
-	panicker()
-	fmt.Println("end")
 
 }
-
-func panicker() {
-	fmt.Println("about to panic")
-	defer func() {
-		if err := recover(); err != nil {
-			log.Println("err", err)
-		}
-	}()
-	panic("something bad happened")
-	log.Println("after panic")
-}
-
-// Defer
-// Used to delay execution of a statement until dunction exits
-// Useful to group "open" and "close" functions together
-// Run in LIFO
-// Arguments evaluated at time defer is executed, not at time of called function execution
-
-// Panic
-// Occurs when program cannot continue at all
-// cannot obtain TCP port for web server
-// if nothing handles panic, program will exit
-
-// Recover
-// Used to recover from panics
-// Only useful in deferred functions
-// Current function will not attempt to
-// continue , but higher functions in call stack
-// will
-
-// import "net/http"
-//
-// func main(){
-// http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request))
-// w.write([]byte("Hello Go!"))
-// }
-// err := http.ListenAndServe(":8000",nill)
-// if err != nill {
-// panic(err.Error())
-// }
-//
